05c3e2010885e7f7d3f0e20b01aac1cfe580fb22
Steven Sojka <steelsojka@gmail.com>
2012-07-30 09:04:07 -0500

    Added parameter for firing an event once.

--- src/EventRegistry.js
+++ src/EventRegistry.js
@@ -1,18 +1,18 @@
 /**
  * EventRegistry.js v1.0.0
  * @author Steven Sojka
- * 
- * This module extends EventEmitter.js to allow event binding to 
+ *
+ * This module extends EventEmitter.js to allow event binding to
  * classes or objects and register them to a specific event emitter.
  *
  * https://github.com/steelsojka/EventRegistry
- * 
+ *
  * - Licensed under the MIT license
- * 
+ *
  * @extends {EventEmitter}
  * @requires EventEmitter.js
  * @example
- * 
+ *
  * var Person = function(name) {
  *   this.name = name;
  * }
@@ -25,12 +25,12 @@
  *     alert("Nice to meet you! I'm " + this.name);
  *   }
  * }
- * 
+ *
  * var myEmitter = new EventEmitter();
  *
  * // This classes prototype is now extended with on, emit and bind methods
  * // that are directing to myEmitter instance
- *  
+ *
  * myEmitter.register(Person);
  *
  * var me = new Person("Steven");
@@ -44,12 +44,12 @@
  * me.on('reply', function() {
  *   this.reply();
  * });
- * 
+ *
  * you.emit('greet');
- * 
+ *
  * // alerts "Hi, my name is David"
  * // second alert "Nice to meet you! I'm Steven"
- * 
+ *
  */
 
 ;var EventEmitter = (function(EventEmitter) {
@@ -63,21 +63,23 @@
    * Registers an object or object instance to a specific event emitter.
    * @param  {Function|Object|Array} object Object, Function class, or Array of objects
    *                                        that will be tied to an event emitter.
-   * @return {Null}        
+   * @return {Null}
    */
   EE_PROTO.register = function(object) {
     var self = this;
     var _prototype = {
-      on : function(event, listener) {
-        self.on.call(self, event, listener, this);
+      on : function(event, listener, scope, once) {
+        var _scope = scope || this;
+        self.on.call(self, event, listener, _scope, once);
       },
       emit : function() {
         self.emit.apply(self, arguments);
-      },
-      bind : function(event, listener) {
-        self.on.call(self, event, listener, this);
       }
     };
+
+    //Alias for on
+    _prototype.bind = _prototype.on;
+
     if(_isArray(object)) {
       for (var i = object.length - 1; i >= 0; i--) {
         _setProto(object[i], _prototype);
@@ -98,7 +100,7 @@
   // Sets either the prototype if object is a function or
   // adds to an object if an instance.
   var _setProto = function(object, _prototype) {
-    if(typeof object === "function") 
+    if(typeof object === "function")
       object.prototype = _extend(object.prototype, _prototype);
     else if(typeof object === "object")
       object = _extend(object, _prototype);
